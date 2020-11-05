48f8aa8dad5f349f6fea3f845ab4e27a4971a388
singsong19 <69883408+singsong19@users.noreply.github.com>
2020-11-05 10:38:24 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// 코드를 작성해 주세요.
+// 에스프레소 : 10 kCal
+// 우유 : 170 kCal
+// 초코시럽 : 50 kCal
+// 휘핑크림 : 60 kCal
+
+let espresso = 10;
+let milk = 170;
+let chocolateSyrup = 50;
+let whippedCream = 60;
+// 메뉴별 칼로리 테스트
+console.log(espresso); // 에스프레소 칼로리
+console.log(espresso + milk); // 라떼 칼로리
+console.log(espresso + chocolateSyrup + milk); // 모카 칼로리
+console.log(espresso + chocolateSyrup + milk + whippedCream); // 모카(휘핑 추가) 칼로리
+
